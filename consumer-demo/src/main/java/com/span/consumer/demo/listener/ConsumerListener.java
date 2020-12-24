c3e9294c87bb102872cf8bedfc8a2a80035cc70e
Artemis-ion <1456721424@qq.com>
2020-12-24 16:39:27 +0800

    消费者确认、生产者确认，避免消息丢失

--- consumer-demo/src/main/java/com/span/consumer/demo/listener/ConsumerListener.java
+++ consumer-demo/src/main/java/com/span/consumer/demo/listener/ConsumerListener.java
@@ -1,11 +1,15 @@
 package com.span.consumer.demo.listener;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author Span
@@ -19,7 +23,27 @@ public class ConsumerListener {
             exchange = @Exchange(value = "SPRING_RABBIT_EXCHANGE", type = ExchangeTypes.TOPIC, ignoreDeclarationExceptions = "true"),
             key = {"a.b"}
     ))
-    public void listener(String msg) {
-        System.out.println("消费者收到信息" + msg);
+    public void listener(String msg, Channel channel, Message message) throws IOException {
+        try {
+            System.out.println("消费者收到信息" + msg);
+
+            long deliveryTag = message.getMessageProperties().getDeliveryTag();
+            channel.basicAck(deliveryTag, true);
+            //设置异常
+            //int i = 1/0;
+            System.out.println("=================");
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            //获取重复投递的状态
+            if (message.getMessageProperties().getRedelivered()){
+                //如果重复投递过了，就不重试，直接拒绝  队列中的消息直接清空
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+            } else {
+                //如果没有重复投递，就重试
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            }
+
+        }
     }
 }
---
+++ producer-demo/src/main/java/com/span/producer/demo/config/RabbitConfig.java
@@ -0,0 +1,51 @@
+package com.span.producer.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Span
+ * @date 2020/12/24  15:43
+ * @description
+ */
+@Configuration
+@Slf4j
+public class RabbitConfig {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 为了让这个方法生效，使用@PostConstruct注解
+     */
+    @PostConstruct
+    public void init() {
+        //不管消息有没有到达交换机，都执行该回调
+        //需要的参数是一个函数式接口，使用lambda表达式
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (!ack) {
+                //如果没有到达交换机
+                log.error("消息没有到达交换机, 响应数据是{}, 错误原因是{}", correlationData, cause);
+            } else {
+                //消息到达队列
+                log.info("消息到达队列！！！");
+            }
+
+        });
+
+        //如果消息没有达到队列，才会执行
+        rabbitTemplate.setReturnCallback((message,replyCode,replyText, exchange, routingKey)-> {
+            //message返回的是一个二进制数组
+            log.error("消息没有到达队列, 交换机:{}, 路由键：{}, 返回的文字：{}, 消息内容：{}"
+                    , exchange, routingKey, replyText, new String(message.getBody()));
+        });
+    }
+
+
+}
